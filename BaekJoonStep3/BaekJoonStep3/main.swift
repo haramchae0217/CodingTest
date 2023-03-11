8603a7c94661bdde2907511c450f96ebdf7e49b7
Chae Haram <djvemfla2@gmail.com>
2023-03-11 18:25:29 +0900

    백준 코테 스텝3 2번문제

--- BaekJoonStep3/BaekJoonStep3/main.swift
+++ BaekJoonStep3/BaekJoonStep3/main.swift
@@ -27,3 +27,32 @@ import Foundation
 //for i in 1...9 {
 //    print("\(dan) * \(i) = \(dan * i)")
 //}
+
+// 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//       각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력 : 각 테스트 케이스 마다 A + B를 출력한다.
+/* 예제 입력1 :
+ 5
+ 1 1
+ 2 3
+ 3 4
+ 9 8
+ 5 2 */
+/* 예제 출력2 :
+ 2
+ 5
+ 7
+ 17
+ 7 */
+// 풀이 :
+//let count = Int(readLine()!)!
+//var caseArr: [(Int, Int)] = []
+//for _ in 0...count - 1 {
+//    let line = readLine()!
+//    let lineArr = line.components(separatedBy: " ")
+//    caseArr.append((Int(lineArr[0])!, Int(lineArr[1])!))
+//}
+//for i in 0...count - 1 {
+//    print(caseArr[i].0+caseArr[i].1)
+//}
