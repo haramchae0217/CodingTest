5448ccb38958408cea4f5e4a20130dfe348bc020
Chae Haram <djvemfla2@gmail.com>
2023-03-11 18:28:00 +0900

    백준 코테 스텝3 3번문제

--- BaekJoonStep3/BaekJoonStep3/main.swift
+++ BaekJoonStep3/BaekJoonStep3/main.swift
@@ -56,3 +56,16 @@ import Foundation
 //for i in 0...count - 1 {
 //    print(caseArr[i].0+caseArr[i].1)
 //}
+
+// 문제 : n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 n (1 <= n <= 10,000)이 주어진다.
+// 출력 : 1부터 n까지 합을 출력한다.
+// 예제 입력1 : 3
+// 예제 출력1 : 6
+// 풀이 :
+//let n = Int(readLine()!)!
+//var sum = 0
+//for i in 1...n {
+//    sum += i
+//}
+//print(sum)
