036efc6c782173690183522ede0960cbc14a0975
Chae Haram <djvemfla2@gmail.com>
2023-03-11 16:16:43 +0900

    백준 코딩 스텝 2

---
+++ BaekJoonStep2/BaekJoonStep2/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  BaekJoonStep2
+//
+//  Created by Chae_Haram on 2023/03/11.
+//
+
+// BaekJoon Coding Test Step2
+import Foundation
+
+// 문제 : 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+// 입력 : 두 자연수 A와 B가 주어진다.
+// 출력 : 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+// 예제 입력 : 7 3
+// 예제 출력 : 10 4 21 2 1
