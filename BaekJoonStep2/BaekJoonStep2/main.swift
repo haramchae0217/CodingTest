79891c3e44c84ba14c748fa6377e4ee3b9361594
Chae Haram <djvemfla2@gmail.com>
2023-03-11 17:49:32 +0900

    백준 코테 스텝2 7번문제

--- BaekJoonStep2/BaekJoonStep2/main.swift
+++ BaekJoonStep2/BaekJoonStep2/main.swift
@@ -167,22 +167,63 @@ import Foundation
 // 예제 입력3 : 23 48\n25
 // 예제 출력3 : 0 13
 // 풀이 :
+//let line = readLine()!
+//let spendTime = Int(readLine()!)!
+//
+//let timeArr = line.components(separatedBy: " ")
+//let hour = Int(timeArr[0])!
+//let min = Int(timeArr[1])!
+//
+//if (hour >= 0 && hour <= 23 && min >= 0 && min <= 59 && spendTime >= 0 && spendTime <= 1000) {
+//    if(min + spendTime < 60) {
+//        print("\(hour) \(min + spendTime)")
+//    } else {
+//        let a = (min + spendTime) / 60
+//        if(hour + a > 23) {
+//            print("\(hour + a - 24) \(min + spendTime - (60 * a))")
+//        } else {
+//            print("\(hour + a) \(min + spendTime - (60 * a))")
+//        }
+//    }
+//}
+
+/* 문제 :
+ 1에서부터 6까지의 눈을 가진 3개의 주사위를 던져서 다음과 같은 규칙에 따라 상금을 받는 게임이 있다.
+
+    1. 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+    2. 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+    3. 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
+ 예를 들어, 3개의 눈 3, 3, 6이 주어지면 상금은 1,000+3×100으로 계산되어 1,300원을 받게 된다. 또 3개의 눈이 2, 2, 2로 주어지면 10,000+2×1,000 으로 계산되어 12,000원을 받게 된다. 3개의 눈이 6, 2, 5로 주어지면 그중 가장 큰 값이 6이므로 6×100으로 계산되어 600원을 상금으로 받게 된다.
+
+ 3개 주사위의 나온 눈이 주어질 때, 상금을 계산하는 프로그램을 작성 하시오.*/
+// 입력 : 첫째 줄에 3개의 눈이 빈칸을 사이에 두고 각각 주어진다.
+// 출력 : 첫째 줄에 게임의 상금을 출력 한다.
+// 예제 입력1 : 3 3 6
+// 예제 출력1 : 1300
+// 예제 입력2 : 2 2 2
+// 예제 출력2 : 12000
+// 예제 입력3 : 6 2 5
+// 예제 출력3 : 600
+
 let line = readLine()!
-let spendTime = Int(readLine()!)!
+let lineArr = line.components(separatedBy: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+let c = Int(lineArr[2])!
 
-let timeArr = line.components(separatedBy: " ")
-let hour = Int(timeArr[0])!
-let min = Int(timeArr[1])!
-
-if (hour >= 0 && hour <= 23 && min >= 0 && min <= 59 && spendTime >= 0 && spendTime <= 1000) {
-    if(min + spendTime < 60) {
-        print("\(hour) \(min + spendTime)")
+if(a >= 1 && a <= 6 && b >= 1 && b <= 6 && c >= 1 && c <= 6 ) {
+    if(a == b && b == c) {
+        print(10000 + a * 1000)
+    } else if(a != b && b != c && a != c) {
+        let sortLine = lineArr.sorted(by: > )
+        print(Int(sortLine[0])! * 100)
     } else {
-        let a = (min + spendTime) / 60
-        if(hour + a > 23) {
-            print("\(hour + a - 24) \(min + spendTime - (60 * a))")
+        if(a == b) {
+            print(1000 + a * 100)
+        } else if(b == c) {
+            print(1000 + b * 100)
         } else {
-            print("\(hour + a) \(min + spendTime - (60 * a))")
+            print(1000 + c * 100)
         }
     }
 }
