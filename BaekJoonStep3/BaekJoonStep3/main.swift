b7b643c9161958b54a6f810c1390948650faf674
Chae Haram <djvemfla2@gmail.com>
2023-03-15 13:38:52 +0900

    백준 코테 스텝3 7번 문제

--- BaekJoonStep3/BaekJoonStep3/main.swift
+++ BaekJoonStep3/BaekJoonStep3/main.swift
@@ -201,4 +201,24 @@ import Foundation
 //    print(a)
 //}
 
+// 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 테스트 케이스의 개수T가 주어진다.
+//       각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력 : 각 테스트 케이스 마다 "Case #x : "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
 
+let count = Int(readLine()!)!
+var addArr: [(Int,Int)] = []
+
+for _ in 0...count - 1 {
+    let line = readLine()!
+    let lineArr = line.components(separatedBy: " ")
+    let a = Int(lineArr[0])!
+    let b = Int(lineArr[1])!
+    if(a > 0 && b < 10) {
+        addArr.append((a,b))
+    }
+}
+
+for i in 0...count - 1 {
+    print("Case #\(i+1): \(addArr[i].0 + addArr[i].1)")
+}
