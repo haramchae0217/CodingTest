9847b12292225eae6ed90bd699727cc16413aa0f
Chae Haram <djvemfla2@gmail.com>
2023-03-15 14:03:00 +0900

    백준 코테 스텝3 10번 문제

--- BaekJoonStep3/BaekJoonStep3/main.swift
+++ BaekJoonStep3/BaekJoonStep3/main.swift
@@ -250,10 +250,27 @@ import Foundation
 // 입력 : 첫째 줄에 N(1 <= N <= 100)이 주어진다.
 // 출력 : 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
 // 풀이 :
+//let n = Int(readLine()!)!
+//
+//for i in 1...n {
+//    for j in 1...i {
+//        print("*", terminator: "")
+//    }
+//    print("")
+//}
+
+// 문제 : 첫째 줄에늘 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+//       하지만, 오른쪽을 기준으로 정렬한 별을 출력하시오.
+// 입력 : 첫째 줄에 N(1 <= N <= 100)이 주어진다.
+// 출력 : 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+// 풀이 :
 let n = Int(readLine()!)!
 
-for i in 1...n {
-    for j in 1...i {
+for i in 1 ... n {
+    for _ in 0 ..< (n-i) {
+        print(" ", terminator: "")
+    }
+    for _ in 0 ..< i {
         print("*", terminator: "")
     }
     print("")
