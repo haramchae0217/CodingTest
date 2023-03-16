e696439f0ad3a5edd1145e5ea684dd0bd9ef1a05
Chae Haram <djvemfla2@gmail.com>
2023-03-16 13:23:33 +0900

    백준 코테 스텝3 12번 문제

--- BaekJoonStep3/BaekJoonStep3/main.swift
+++ BaekJoonStep3/BaekJoonStep3/main.swift
@@ -276,25 +276,41 @@ import Foundation
 //    print("")
 //}
 
-// 문제 : 두 정수 A와 B를 입력 받은 다음 A+B를 출력하는 프로그램을 작성하시오.
+// 문제 : 두 정수 A와 B를 입력 받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 /* 입력 : 입력은 여러개의 테스트 케이스로 이루어져 있다.
  각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
  입력의 마지막에는 0 두개가 들어온다. */
 // 출력 : 각 테스트 케이스마다 A+B를 출력한다.
 // 풀이 :
+//var caseArr: [(Int, Int)] = []
+//while(true) {
+//    let line = readLine()!
+//    let lineArr = line.components(separatedBy: " ")
+//    let a = Int(lineArr[0])!
+//    let b = Int(lineArr[1])!
+//    if (a == 0 && b == 0) {
+//        break;
+//    } else {
+//        caseArr.append((a,b))
+//    }
+//}
+//for i in 0...caseArr.count - 1 {
+//    print(caseArr[i].0 + caseArr[i].1)
+//}
+ 
+// 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력 : 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+//       각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력 : 각 테스트 케이스 마다 A+B를 출력한다.
+// 풀이 :
 var caseArr: [(Int, Int)] = []
-while(true) {
-    let line = readLine()!
+while let line = readLine() {
     let lineArr = line.components(separatedBy: " ")
     let a = Int(lineArr[0])!
     let b = Int(lineArr[1])!
-    if (a == 0 && b == 0) {
-        break;
-    } else {
-        caseArr.append((a,b))
-    }
+    caseArr.append((a,b))
 }
 for i in 0...caseArr.count - 1 {
     print(caseArr[i].0 + caseArr[i].1)
 }
- 
+
